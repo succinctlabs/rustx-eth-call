da1c2c23ab69713af44575bd89ad682c26f7fa23
John Guibas <john@succinct.xyz>
2023-10-12 14:44:16 -0700

    fix: call error

--- contracts/src/EthCallOracleV1.sol
+++ contracts/src/EthCallOracleV1.sol
@@ -2,22 +2,35 @@
 pragma solidity ^0.8.16;
 
 interface IFunctionGateway {
-    function request(
-        bytes32 functionId,
-        bytes memory inputs,
-        bytes4 callbackSelector,
-        bytes memory context
+    function requestCallback(
+        bytes32 _functionId,
+        bytes memory _input,
+        bytes memory _context,
+        bytes4 _callbackSelector,
+        uint32 _callbackGasLimit
+    ) external payable returns (bytes32);
+
+    function requestCall(
+        bytes32 _functionId,
+        bytes memory _input,
+        address _address,
+        bytes memory _data,
+        uint32 _gasLimit
     ) external payable;
+
+    function verifiedCall(bytes32 _functionId, bytes memory _input)
+        external
+        view
+        returns (bytes memory);
 }
 
 contract EthCallOracleV1 {
     /// @notice The address of the function gateway.
-    address public constant FUNCTION_GATEWAY =
-        0x852a94F8309D445D27222eDb1E92A4E83DdDd2a8;
+    address public constant FUNCTION_GATEWAY = 0xE304f6B116bE5e43424cEC36a5eFd0B642E0dC95;
 
     /// @notice The function id of the eth call oracle.
     bytes32 public constant FUNCTION_ID =
-        0x3a935850aecabccfd63d22f90cc844fff3f3ee63cc08c62cd4105e14954b744d;
+        0x197e4275632346e0d02a22fcb54b6db1966fd734325b2ea40fef04485ded2163;
 
     /// @notice The nonce of the oracle.
     uint256 public nonce = 0;
@@ -26,26 +39,59 @@ contract EthCallOracleV1 {
     event EthCallOracleV1Update(uint256 requestId, bytes result);
 
     /// @notice The entrypoint for requesting an oracle update.
-    function request(
+    function requestCallback(
         uint32 chainId,
         uint64 blockNumber,
         address fromAddress,
         address toAddress,
         bytes memory data
     ) external payable {
-        IFunctionGateway(FUNCTION_GATEWAY).request{value: msg.value}(
+        IFunctionGateway(FUNCTION_GATEWAY).requestCallback{value: msg.value}(
             FUNCTION_ID,
             abi.encode(chainId, blockNumber, fromAddress, toAddress, data),
-            this.handleUpdate.selector,
-            abi.encode(nonce)
+            abi.encode(nonce),
+            this.handleCallback.selector,
+            2000000
         );
         nonce++;
     }
 
     /// @notice The callback function for the oracle.
-    function handleUpdate(bytes memory output, bytes memory context) external {
+    function handleCallback(bytes memory output, bytes memory context) external {
         require(msg.sender == FUNCTION_GATEWAY);
         uint256 requestId = abi.decode(context, (uint256));
         emit EthCallOracleV1Update(requestId, output);
+    }
+
+    /// @notice The entrypoint for requesting an oracle update.
+    function requestCall(
+        uint32 chainId,
+        uint64 blockNumber,
+        address fromAddress,
+        address toAddress,
+        bytes memory data
+    ) external payable {
+        IFunctionGateway(FUNCTION_GATEWAY).requestCall{value: msg.value}(
+            FUNCTION_ID,
+            abi.encode(chainId, blockNumber, fromAddress, toAddress, data),
+            address(this),
+            abi.encodeWithSelector(
+                this.entrypoint.selector, chainId, blockNumber, fromAddress, toAddress, data
+            ),
+            1000000
+        );
+    }
+
+    function entrypoint(
+        int32 chainId,
+        uint64 blockNumber,
+        address fromAddress,
+        address toAddress,
+        bytes memory data
+    ) external {
+        bytes memory output = IFunctionGateway(FUNCTION_GATEWAY).verifiedCall(
+            FUNCTION_ID, abi.encode(chainId, blockNumber, fromAddress, toAddress, data)
+        );
+        emit EthCallOracleV1Update(0, output); 
     }
 }
